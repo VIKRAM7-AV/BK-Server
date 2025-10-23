f12ba41f1792dbf6a66b574c2144536d981f3c58
VIKRAM <vikramria195@gmail.com>
2025-10-24 00:29:21 +0530

    Test

--- Controller/NotificationController.js
+++ Controller/NotificationController.js
@@ -1,5 +1,4 @@
-
-import User from "../Model/UserModel.js";
+import AgentNotification from "../Model/AgentNotification.js";
 import Notification from "../Model/notification.js";
 
 
@@ -18,6 +17,34 @@ export const DelNotification = async (req, res) => {
     try {
         const { id } = req.params;
         const deletedNotification = await Notification.findByIdAndDelete(id);
+        if (!deletedNotification) {
+            return res.status(404).json({ message: "Notification not found" });
+        }
+        res.status(200).json({ message: "Notification deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+        console.error("Error deleting notification:", error);
+    }
+}
+
+
+export const GetNotificationAgent = async (req, res) => {
+    try {
+        const { agentId } = req.params;
+        const notifications = await AgentNotification.find({ agentId : agentId }).sort({ createdAt: -1 });
+        res.status(200).json({ notifications });        
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+        console.error("Error fetching notifications:", error);        
+    }
+}
+
+
+
+export const DelNotificationAgent = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedNotification = await AgentNotification.findByIdAndDelete(id);
         if (!deletedNotification) {
             return res.status(404).json({ message: "Notification not found" });
         }
--- Routes/getNotification.js
+++ Routes/getNotification.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { GetNotifications, DelNotification } from "../Controller/NotificationController.js";
+import { GetNotifications, DelNotification, GetNotificationAgent, DelNotificationAgent } from "../Controller/NotificationController.js";
 
 
 const router = express.Router();
@@ -7,5 +7,8 @@ const router = express.Router();
 
 router.get("/notifications/:userId", GetNotifications);
 router.delete("/delnotification/:id", DelNotification);
+router.get("/agent/notifications/:agentId", GetNotificationAgent);
+router.delete("/agent/delnotification/:id", DelNotificationAgent);
+
 
 export default router;
